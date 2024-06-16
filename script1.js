d20c7972598ce83031191ed9505c80bb69cc6619
sanita27 <117376431+sanita27@users.noreply.github.com>
2024-06-16 19:21:57 +0000

    commit	new file:   RSB_DataSet.csv commit	new file:   chart1.ipynb commit	new file:   chart1.json commit	new file:   chart2.json commit	new file:   chart3.json commit	new file:   chart4.json commit	new file:   chart5.json commit	new file:   chart6.json commit	new file:   chart7.json commit	new file:   countries.geojon commit	new file:   data.ipynb commit	modified:   index.html commit	new file:   merged_data.csv commit	new file:   merged_data_with_year.csv commit	new file:   preprocessed_merged_data_with_year.csv commit	deleted:    s2_chart1.json commit	new file:   script1.js

---
+++ script1.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    vegaEmbed('#bar_chart', 'chart1.json').catch(console.error);
+    vegaEmbed('#scatter_plot', 'chart2.json').catch(console.error);
+    vegaEmbed('#political', 'chart3.json').catch(console.error);
+    vegaEmbed('#map_chart', 'chart4.json').catch(console.error);
+    vegaEmbed('#legal', 'chart5.json').catch(console.error);
+    vegaEmbed('#safety', 'chart6.json').catch(console.error);
+    vegaEmbed('#culture', 'chart7.json').catch(console.error);
+
+});
